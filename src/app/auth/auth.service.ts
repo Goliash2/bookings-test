2f95e99e89e199a38a7fcee787da35d2bbc82e9a
Petr Hnyk <hnyk.petr@centrum.cz>
2020-01-18 19:32:31 +0100

    added custom styling and offer-item component to use component level styling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private innerUserIsAuthenticated = false;
+  private innerUserIsAuthenticated = true;
   get userIsAuthenticated() {
     return this.innerUserIsAuthenticated;
   }
